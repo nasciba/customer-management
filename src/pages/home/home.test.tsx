37b9229d1c756cf58fc98ca37fcc24f4b80faf14
Amanda Batista <amanda.batista1@bp.com>
2023-10-22 17:28:45 -0300

    Add unit tests to home page

--- src/pages/home/home.test.tsx
+++ src/pages/home/home.test.tsx
@@ -13,5 +13,17 @@ describe("Home Page", () => {
     render(<Home />);
     expect(screen.getByRole("columnheader", { name: "Company" })).toBeInTheDocument();
   })
+  it("should render the company name correctly in the table", ()=> {
+    render(<Home />);
+    expect(screen.getByRole("cell", { name: "Doyle-Kessler" })).toBeInTheDocument();
+  })
+  it("should render the industry name correctly in the table", ()=> {
+    render(<Home />);
+    expect(screen.getByRole("cell", { name: "travel" })).toBeInTheDocument();
+  })
+  it("should render the number of projects correctly in the table", ()=> {
+    render(<Home />);
+    expect(screen.getByRole("cell", { name: "3" })).toBeInTheDocument();
+  })
  
 });
