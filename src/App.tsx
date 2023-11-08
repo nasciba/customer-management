37e18e97406f293056786343bb46faa3d917286c
Amanda Batista <amanda.batista1@bp.com>
2023-11-08 19:33:24 -0300

    Added route to InactiveCustomers

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AddCustomerPage from "./pages/add-customer/add-customer";
 import Home from "./pages/home/home";
+import InactiveCustomersPage from "./pages/inactive-users/inactive-customers";
 
 const App = () => {
   return (
@@ -10,8 +11,12 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/add-customer" element={<AddCustomerPage/>} />
+          <Route path="/add-customer" element={<AddCustomerPage />} />
           <Route path="/edit-customer" element={<div>Edit Customer</div>} />
+          <Route
+            path="/inactive-customers"
+            element={<InactiveCustomersPage />}
+          />
         </Routes>
       </Router>
     </div>
