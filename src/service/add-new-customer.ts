415f897f2ba16a691fbbee3ff54e842b80651fd7
Amanda Batista <amanda.batista1@bp.com>
2023-11-12 11:10:35 -0300

    Fixed argument passed to axios

--- src/service/add-new-customer.ts
+++ src/service/add-new-customer.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { CustomerDataDto } from '../dtos/customer-data-dto';
 
 const addNewCustomer = async (customerData  : CustomerDataDto) : Promise<AxiosResponse>=> {
-    return axios.post(`${process.env.REACT_APP_PORT_SERVER}/customers`, { body: customerData });
+    return axios.post(`${process.env.REACT_APP_PORT_SERVER}/customers`, customerData );
 }
 
 export default addNewCustomer;
--- src/service/edit-customer.ts
+++ src/service/edit-customer.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosResponse } from 'axios';
 import { CustomerDataDto } from '../dtos/customer-data-dto';
 
-const editCustomer = async (id: string, body: CustomerDataDto) : Promise<AxiosResponse>=> {
-    return axios.put(`${process.env.REACT_APP_PORT_SERVER}/customers/${id}`, { body: body });
+const editCustomerService = async (id: string, body: CustomerDataDto) : Promise<AxiosResponse>=> {
+    return axios.put(`${process.env.REACT_APP_PORT_SERVER}/customers/${id}`, body);
 }
 
-export default editCustomer;
+export default editCustomerService;
