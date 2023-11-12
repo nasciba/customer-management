e33102298aaaf35e0e03744234b66e51e560b1dd
Amanda Batista <amanda.batista1@bp.com>
2023-11-12 11:11:18 -0300

    Fixed getCustomerById return type

--- src/service/get-customer.tsx
+++ src/service/get-customer.tsx
@@ -1,7 +1,7 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { CustomerDataDto } from '../dtos/customer-data-dto';
 
-const getCustomerById = async (id: string) : Promise<AxiosResponse<CustomerDataDto>>=> {
+const getCustomerById = async (id: string) : Promise<CustomerDataDto>=> {
     const response = await axios.get(`${process.env.REACT_APP_PORT_SERVER}/customers/${id}`);
     return response.data;
 }
