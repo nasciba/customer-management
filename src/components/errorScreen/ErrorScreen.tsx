fdf1c11b6f680e75282d122bb4490c81e2ddf1a5
Amanda Batista <amanda.batista1@bp.com>
2023-11-12 11:06:09 -0300

    Added Error component

---
+++ src/components/errorScreen/ErrorScreen.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+
+const ErrorScreen = () => {
+    return ( <Box
+        width="100%"
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <h2>Something went wrong.</h2>
+      </Box> );
+}
+
+export default ErrorScreen
