efff91f727c92edef234958ba649b4ccfdbd90ee
Amanda Batista <amanda.batista1@bp.com>
2023-11-12 11:06:35 -0300

    Added Loading component

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,15 +1,17 @@
-import { Box, CircularProgress } from "@mui/material"
+import { Box, CircularProgress } from "@mui/material";
 
 const Loading = () => {
+  return (
     <Box
-    width="100%"
-    height="100vh"
-    display="flex"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <CircularProgress />
-  </Box>
-}
+      width="100%"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
 
 export default Loading;
