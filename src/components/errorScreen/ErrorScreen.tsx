b86bec25d67dcd90f0cd73bbe74d5d0058b967ef
Amanda Batista <amanda.batista1@bp.com>
2023-11-13 23:10:26 -0300

    Error screen styling

--- src/components/errorScreen/ErrorScreen.tsx
+++ src/components/errorScreen/ErrorScreen.tsx
@@ -1,15 +1,30 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+import { Link } from "react-router-dom";
+import "./errorScreen.scss";
 
 const ErrorScreen = () => {
-    return ( <Box
-        width="100%"
-        height="100vh"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <h2>Something went wrong.</h2>
-      </Box> );
-}
+  return (
+    <Box
+      width="100%"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <SentimentVeryDissatisfiedIcon sx={{ fontSize: 100 }} />
+      <Typography className="error-screen-text" variant="h3">
+        Something went wrong.
+      </Typography>
+      <Typography className="error-screen-text" variant="h5">
+        Please try again later.
+      </Typography>
+      <Link to="/" className="error-screen-text">
+        Go back to home page
+      </Link>
+    </Box>
+  );
+};
 
-export default ErrorScreen
+export default ErrorScreen;
