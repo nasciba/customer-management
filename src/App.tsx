b16df2336ae60953ea1f95285bf0b33dc948aee0
Amanda Batista <amanda.batista1@bp.com>
2023-11-13 21:34:58 -0300

    Added lazy loading and navbar to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React,  { lazy } from "react";
+import React, { lazy, Suspense } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import theme from "./Theme";
+import Loading from "./components/loading/Loading";
+import Navbar from "./components/navbar/Navbar";
 
 const AddCustomerPage = lazy(() => import("./pages/addCustomer/AddCustomer"));
 const HomePage = lazy(() => import("./pages/home/Home"));
-const EditCustomerPage = lazy(() =>
-  import("./pages/editCustomer/EditCustomer")
+const EditCustomerPage = lazy(
+  () => import("./pages/editCustomer/EditCustomer")
 );
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/add-customer" element={<AddCustomerPage />} />
-          <Route path="/edit-customer/:id" element={<EditCustomerPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Suspense fallback={<Loading />}>
+       <Navbar />
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/add-customer" element={<AddCustomerPage />} />
+            <Route path="/edit-customer/:id" element={<EditCustomerPage />} />
+          </Routes>
+        </Router>
+      </Suspense>
+    </ThemeProvider>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,19 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import Loading from "./components/loading/Loading";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Suspense fallback={<Loading/>}>
     <Provider store={store}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </Provider>
-  </Suspense>
 );
 
 
