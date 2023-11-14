7df6e1edeed36c4831590b159fda7e18a95f824b
Amanda Batista <amanda.batista1@bp.com>
2023-11-13 23:16:23 -0300

    Navbar component

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import LogoSvg from "../LogoSvg";
+import { Button } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Button href="/">
+            <LogoSvg />
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
