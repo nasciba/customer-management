dc2d2dce3037f9b215a72999d511d484ad12a87b
Amanda Batista <amanda.batista1@bp.com>
2023-11-13 23:09:48 -0300

    Created Dialog component

---
+++ src/components/dialog/dialog.tsx
@@ -0,0 +1,55 @@
+import {
+  Button,
+  Dialog as MuiDialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+
+interface DialogProps {
+  customerToDelete?: string;
+  open: boolean;
+  text: string;
+  title: string;
+  handleClose: () => void;
+  handleSubmit: () => Promise<void>;
+}
+const Dialog = ({
+  customerToDelete,
+  open,
+  text,
+  title,
+  handleClose,
+  handleSubmit,
+}: DialogProps) => {
+  return (
+    <MuiDialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {text}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="outlined" color="secondary" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleSubmit}
+          autoFocus
+        >
+          Agree
+        </Button>
+      </DialogActions>
+    </MuiDialog>
+  );
+};
+export default Dialog;
